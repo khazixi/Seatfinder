ee280ac8ffae5ded208660dfd2ccdcbb268f7b5c
Khazixi <khazixfangirl@gmail.com>
2024-02-28 17:04:10 -0500

    Added Favicons and Manifest

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,26 @@ export default defineConfig({
   plugins: [
     VitePWA({
       strategies: 'generateSW',
-      injectRegister: 'auto'
+      injectRegister: 'auto',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Seatfinder',
+        short_name: 'Seatfinder',
+        description: "Seatfinder is the best app for seding your location in one of UMass' dining halls to your friends ",
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon512',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     })
   ]
 })
